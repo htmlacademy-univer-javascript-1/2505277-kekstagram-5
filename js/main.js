5db5ccced67d3fc65e3c89924dd310daa8208356
Vika Usova <getsbistorm1@mail.ru>
2024-12-16 17:54:49 +0700

    feat:open big photo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 
 import {creatingMiniatures} from "./thumbnail-renderer.js";
+import {openBigPicture,renderComments} from "./rendering-fullsize-window.js";
+import {createCommentsArray} from "./comment-generator.js";
 
 console.log(creatingMiniatures());
+console.log(openBigPicture());
+console.log(createCommentsArray());
 
